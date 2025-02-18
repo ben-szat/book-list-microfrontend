88f41998853da56e0bceccdbd405e9700d89a73b
ben-szat <ben.szatmari@gmail.com>
2025-02-18 11:47:09 +0100

    base of book list

---
+++ src/BookList.tsx
@@ -0,0 +1,12 @@
+import { Button, Typography } from "@mui/material";
+
+const BookList = () => {
+    return (
+      <>
+        <Typography>This is the book list microfrontend</Typography>
+        <Button>Szia</Button>
+      </>
+    );
+}
+
+export default BookList;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "booklist",
+      filename: "remoteEntry.js", // Ensure this is correct
+      exposes: {
+        "./BookList": "./src/BookList.tsx",
+      },
+      shared: ["react", "react-dom", "@mui/material"],
+    }),
+  ],
+  server: {
+    port: 3001,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  build: {
+    target: "esnext",
+  },
+});
